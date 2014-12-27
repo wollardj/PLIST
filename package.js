25d387ccc3b550c7ad49ede1586c03c920332cd3
Joe Wollard <wollardj@denison.edu>
2014-12-26 23:50:02 -0500

    Remove standard-app-packages and added underscore.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: 'wollardj:plist',
     summary: 'JSON-like interface for converting plist strings to javascript objects and back again.',
-    version: '1.0.0',
+    version: '1.0.1',
     git: 'https://github.com/wollardj/PLIST.git'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('1.0.2.1');
     api.use([
-        'standard-app-packages',
+        'underscore',
         'coffeescript'
     ]);
     api.addFiles([
